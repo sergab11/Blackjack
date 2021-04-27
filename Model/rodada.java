b32a52d23160d1e1408f28c4bf83a6fbd74f4230
Sérgio Gabriel <75457521+sergab11@users.noreply.github.com>
2021-04-27 13:53:28 -0300

    Update rodada.java

--- Model/rodada.java
+++ Model/rodada.java
@@ -1,26 +1,37 @@
 package Model;
+import java.util.ArrayList;
 
 public class rodada {
-	protected fichas [] apostas;
-	protected jogador [] jogadores;
-	protected baralho [] cartas;
+	protected fichas apostas;
+	protected ArrayList<jogador> jogadores = new ArrayList<jogador>();
+	protected ArrayList<baralho> baralhos = new ArrayList<baralho>();
 	
 	public rodada(String jog) {
-		jogadores[0] = new jogador(jog);
+		jogador j0 = new jogador(jog); 
+		jogadores.add(j0);
 	}
 	public rodada(String jog1, String jog2) {
-		jogadores[0] = new jogador(jog1);
-		jogadores[1] = new jogador(jog2);
+		jogador j1 = new jogador(jog1);
+		jogador j2 = new jogador(jog2);
+		jogadores.add(j1);
+		jogadores.add(j2);
 	}
 	public rodada(String jog1, String jog2, String jog3) {
-		jogadores[0] = new jogador(jog1);
-		jogadores[1] = new jogador(jog2);
-		jogadores[2] = new jogador(jog3);
+		jogador j1 = new jogador(jog1);
+		jogador j2 = new jogador(jog2);
+		jogador j3 = new jogador(jog3);
+		jogadores.add(j1);
+		jogadores.add(j2);
+		jogadores.add(j3);
 	}
 	public rodada(String jog1, String jog2, String jog3, String jog4) {
-		jogadores[0] = new jogador(jog1);
-		jogadores[1] = new jogador(jog2);
-		jogadores[2] = new jogador(jog3);
-		jogadores[3] = new jogador(jog4);
+		jogador j1 = new jogador(jog1);
+		jogador j2 = new jogador(jog2);
+		jogador j3 = new jogador(jog3);
+		jogador j4 = new jogador(jog4);
+		jogadores.add(j1);
+		jogadores.add(j2);
+		jogadores.add(j3);
+		jogadores.add(j4);
 	}
 }
