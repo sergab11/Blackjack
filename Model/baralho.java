d348928bcc2c6010c446cca64d822024c8c80daa
Sérgio Gabriel <75457521+sergab11@users.noreply.github.com>
2021-04-27 10:10:03 -0300

    Add files via upload

---
+++ Model/baralho.java
@@ -0,0 +1,8 @@
+package Model;
+
+public class baralho {
+	protected String paus [] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};  
+	protected String ouro [] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+	protected String espada [] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+	protected String copas [] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+}
