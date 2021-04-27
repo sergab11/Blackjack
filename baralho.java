dda7c6ef8a21cd6fd140e86656839d6634f6697c
Sérgio Gabriel <75457521+sergab11@users.noreply.github.com>
2021-04-27 10:07:57 -0300

    Delete baralho.java

--- baralho.java
+++
@@ -1,8 +0,0 @@
-package Model;
-
-public class baralho {
-	protected String paus [] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};  
-	protected String ouro [] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-	protected String espada [] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-	protected String copas [] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-}
