87eabbd78515a931bfb0102395ad7231924b2ee3
Sérgio Gabriel <75457521+sergab11@users.noreply.github.com>
2021-04-29 15:49:12 -0300

    Update rodada.java

--- Model/rodada.java
+++ Model/rodada.java
@@ -2,19 +2,24 @@ package Model;
 import java.util.ArrayList;
 
 public class rodada {
-	protected fichas apostas;
+	protected fichas Apostas;
 	protected ArrayList<jogador> jogadores = new ArrayList<jogador>();
 	protected ArrayList<baralho> baralhos = new ArrayList<baralho>();
+	protected dealer Dealer;
+	static int jogadorVez = 0;
+	static int numj;
 	
 	public rodada(String jog) {
 		jogador j0 = new jogador(jog); 
 		jogadores.add(j0);
+		numj = 1;
 	}
 	public rodada(String jog1, String jog2) {
 		jogador j1 = new jogador(jog1);
 		jogador j2 = new jogador(jog2);
 		jogadores.add(j1);
 		jogadores.add(j2);
+		numj = 2;
 	}
 	public rodada(String jog1, String jog2, String jog3) {
 		jogador j1 = new jogador(jog1);
@@ -23,6 +28,7 @@ public class rodada {
 		jogadores.add(j1);
 		jogadores.add(j2);
 		jogadores.add(j3);
+		numj = 3;
 	}
 	public rodada(String jog1, String jog2, String jog3, String jog4) {
 		jogador j1 = new jogador(jog1);
@@ -33,5 +39,34 @@ public class rodada {
 		jogadores.add(j2);
 		jogadores.add(j3);
 		jogadores.add(j4);
+		numj = 4;
 	}
+	
+	protected static void proximo() {
+		jogadorVez++;
+		if(jogadorVez == numj)
+			jogadorVez = 0;
+	}
+	
+	protected void adicionaAposta() {
+		fichas montante = jogadores.get(jogadorVez).getAposta();
+		Apostas.adicionaConjunto(montante);
+	}
+	
+	public void dobra() {
+		carta recebe = baralhos.get(0).getPrimeiraCarta();
+		jogadores.get(jogadorVez).dobra(recebe);
+	}
+	
+	public void hit() {
+		carta recebe = baralhos.get(0).getPrimeiraCarta();
+		jogadores.get(jogadorVez).hit(recebe);
+	}
+	
+	public void stand() {
+		adicionaAposta();
+		jogadores.get(jogadorVez).limpaAposta();
+		proximo();
+	}
+
 }
