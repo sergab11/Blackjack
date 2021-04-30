ae897e4e23506293b14ea37461b47e2b34154516
Sérgio Gabriel <75457521+sergab11@users.noreply.github.com>
2021-04-30 13:39:40 -0300

    Update Rodada.java

--- model/Rodada.java
+++ model/Rodada.java
@@ -1,11 +1,11 @@
-package Model;
+package model;
 import java.util.ArrayList;
 
 class Rodada {
-	protected fichas Apostas;
-	protected ArrayList<jogador> jogadores = new ArrayList<jogador>();
-	protected ArrayList<baralho> baralhos = new ArrayList<baralho>();
-	protected dealer Dealer;
+	protected Fichas Apostas;
+	protected ArrayList<Jogador> jogadores = new ArrayList<Jogador>();
+	protected ArrayList<Baralho> baralhos = new ArrayList<Baralho>();
+	protected Dealer dealer;
 	static int jogadorVez = 0;
 	static int numj;
 	
@@ -13,11 +13,11 @@ class Rodada {
 		
 		numj = jogadores.size();
 		if (numj > 4) {
-			throw new Exception("O limite de 4 jogadores foi excedido.") 
+			throw new Exception("O limite de 4 jogadores foi excedido."); 
 		}
 		
 		for (int i = 0; i < nomesJogadores.size(); i++) {
-			jogador aux = new Jogador(nomesJogadores[i]);
+			Jogador aux = new Jogador(nomesJogadores.get(i));
 			jogadores.add(aux);
 		}
 	}
@@ -29,17 +29,17 @@ class Rodada {
 	}
 	
 	protected void adicionaAposta() {
-		fichas montante = jogadores.get(jogadorVez).getAposta();
+		Fichas montante = jogadores.get(jogadorVez).getAposta();
 		Apostas.adicionaConjunto(montante);
 	}
 	
 	public void dobra() {
-		carta recebe = baralhos.get(0).getPrimeiraCarta();
+		Carta recebe = baralhos.get(0).getPrimeiraCarta();
 		jogadores.get(jogadorVez).dobra(recebe);
 	}
 	
 	public void hit() {
-		carta recebe = baralhos.get(0).getPrimeiraCarta();
+		Carta recebe = baralhos.get(0).getPrimeiraCarta();
 		jogadores.get(jogadorVez).hit(recebe);
 	}
 	
